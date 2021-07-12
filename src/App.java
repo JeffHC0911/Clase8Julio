04f8ecff3b26b70c3abebfb786757abcb14998a2
tu_usuario_de_GITHUB <jefferhc911@gmail.com>
2021-07-11 23:35:13 -0500

    Commit inicial

---
+++ src/App.java
@@ -0,0 +1,10 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+        System.out.println("Hola mundoo");
+
+        int r = 8;
+        int a = 9;
+        int suma = r + a;
+    }
+}
